c45d00832d9092f77796b54725e7b7f256b1570a
Sherif Raafat <sherifr2ft@outlook.com>
2024-04-30 22:01:57 +0300

    Update SwiftFlutterDeviceIdPlugin.swift

--- ios/Classes/SwiftFlutterDeviceIdPlugin.swift
+++ ios/Classes/SwiftFlutterDeviceIdPlugin.swift
@@ -3,7 +3,7 @@ import UIKit
 
 public class SwiftFlutterDeviceIdPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "flutter_device_id", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: "platform_device_id", binaryMessenger: registrar.messenger())
     let instance = SwiftFlutterDeviceIdPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
